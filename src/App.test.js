620702932f7bebfc364b7bf3d4ffe221ad93d25d
Savitha <savithapremkumar@gmail.com>
2020-10-18 19:26:11 +1300

    [Test fails] Testing that app component renders successfully

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "./../utils";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//import { render } from '@testing-library/react';
+import App from "./App";
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+const setUp = (props = {}) => {
+  return shallow(<App />);
+};
+
+describe("App Component", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it("Should render without error", () => {
+    const wrapper = findByTestAttr(component, "appComponent");
+    //console.log('NODE_ENV', process.env.NODE_ENV);
+    expect(wrapper.length).toBe(1);
+  });
 });
