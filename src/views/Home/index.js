71430ed0b636243a50634089a90a1511ded79377
Savitha <savithapremkumar@gmail.com>
2020-10-18 21:50:39 +1300

    Adding styles for the button in Home view

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export class Home extends Component {
   render() {
     return (
-      <div>
-        <button className="start-button"></button>
+      <div className="button-container">
+        <button className="start-button">Fetch Albums!</button>
       </div>
     );
   }
